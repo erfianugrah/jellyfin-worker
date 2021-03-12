397bbc684ba5fab8a6c71b64b651e780a4c24c8f
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-03-13 07:03:52 +0800

    Removed Audio, Images, Video and frontEnd

--- jellyfin.js
+++ jellyfin.js
@@ -6,9 +6,9 @@ let cacheRequest = newRequest.pathname
 const media = new Map ([
     ['video', /^.*\.(m4s|mp4|ts|avi|mpeg|mpg|mkv|bin|webm|vob|flv|m2ts|mts|3gp|m4v|wmv|qt)/],
     ['image', /^.*\.(jpeg|jpg|png|dng|tiff|webp|gif)/],
-    ['frontEnd', /^.*\.(css)/],
+    ['frontEnd', /^.*\.(css|js)/],
     ['audio', /^.*\.(flac|aac|mp3|alac|aiff|wav|ogg|aiff|opus|ape|wma|3gp)/],
-    ['directPlay', /^.*(\/Download|\/Audio|\/Video)/],
+    ['directPlay', /^.*(\/Download)/],
     ['manifest', /^.*\.(m3u8|mpd)/]
 ])
 
@@ -16,11 +16,11 @@ const media = new Map ([
         return await fetch(newRequest, { cf: {cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '400-499': 5, '500-599': 1 }},
         })
     if (cacheRequest.match(media.get('image')))
-        return await fetch(newRequest, { cf: {cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '400-499': 5, '500-599': 1 }},
+        return await fetch(newRequest, { cf: {cheEverything: true, cacheTtlByStatus: { '200-299': 86400, '400-499': 5, '500-599': 1 }},
         })
-    if (cacheRequest.match(media.get('frontEnd')))
-        return await fetch(newRequest, { cf: {cacheEverything: true, cacheTtlByStatus: { '200-299': 3600, '400-499': 5, '500-599': 1 }},
-        })
+//    if (cacheRequest.match(media.get('frontEnd')))
+//       return await fetch(newRequest, { cf: {cacheEverything: true, cacheTtlByStatus: { '200-299': 3600, '400-499': 5, '500-599': 1 }},
+//        })
     if (cacheRequest.match(media.get('audio')))
         return await fetch(newRequest, { cf: {cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '400-499': 5, '500-599': 1 }},
         })
