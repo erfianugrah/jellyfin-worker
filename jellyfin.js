931a2dc20f238f28bb4f2dedc10f377dbc7ddeb3
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-03-14 17:32:58 +0800

    Fixed Logic

--- jellyfin.js
+++ jellyfin.js
@@ -1,50 +1,57 @@
+addEventListener('fetch', event => {
+    return event.respondWith(handleRequest(event.request));
+})
+
 async function handleRequest(request) {
 
-let newRequest = new URL(request.url)
-let cacheRequest = newRequest.pathname
+const newRequest = new URL(request.url)
+let customCacheKey = newRequest.hostname + newRequest.pathname
 
-const media = new Map ([
-    ['video', /^.*\.(m4s|mp4|ts|avi|mpeg|mpg|mkv|bin|webm|vob|flv|m2ts|mts|3gp|m4v|wmv|qt)/],
-    ['image', /^.*\.(jpeg|jpg|png|dng|tiff|webp|gif)/],
-    ['frontEnd', /^.*\.(css|js)/],
-    ['audio', /^.*\.(flac|aac|mp3|alac|aiff|wav|ogg|aiff|opus|ape|wma|3gp)/],
-    ['directPlay', /^.*(\/Download|\/Audio|\/Items)/],
-    ['manifest', /^.*\.(m3u8|mpd)/]
-])
+const video = /^.*\.(m4s|mp4|ts|avi|mpeg|mpg|mkv|bin|webm|vob|flv|m2ts|mts|3gp|m4v|wmv|qt)/
+const image = /^.*\.(jpeg|jpg|png|dng|tiff|webp|gif)/
+const frontEnd = /^.*\.(css|js)/
+const audio = /^.*\.(flac|aac|mp3|alac|aiff|wav|ogg|aiff|opus|ape|wma|3gp)/
+const directPlay = /^.*(\/Download|\/Audio)/
+const manifest = /^.*\.(m3u8|mpd)/
 
-    if (cacheRequest.match(media.get('video'))) {
-        return await fetch(request, 
-            { cf: {cacheKey: cacheRequest, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 300, '400-499': 1, '500-599': 0 }},
-        })
-    }
-    if (cacheRequest.match(media.get('image'))) {
-        return await fetch(request, 
-            { cf: {cacheKey: cacheRequest, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 300, '400-499': 1, '500-599': 0 }},
-        })
-    }
-    if (cacheRequest.match(media.get('frontEnd'))) {
-        return await fetch(request, 
-            { cf: {cacheKey: cacheRequest, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 300, '400-499': 1, '500-599': 0 }},        
-        })
-    }
-    if (cacheRequest.match(media.get('audio'))) {
-        return await fetch(request, 
-            { cf: {cacheKey: cacheRequest, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 300, '400-499': 1, '500-599': 0 }},
-        })
-    }
-    if (cacheRequest.match(media.get('directPlay'))) {
-        return await fetch(request, 
-            { cf: {cacheKey: cacheRequest, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 300, '400-499': 1, '500-599': 0 }},
-        })
-    }
-    if (cacheRequest.match(media.get('manifest'))) {
-        return await fetch(request, 
-            { cf: {cacheKey: cacheRequest, cacheEverything: true, cacheTtlByStatus: { '200-299': 2, '300-399': 300, '400-499': 1, '500-599': 0 }},
-        })
-    }
-    return await fetch(request)
+
+if (newRequest.pathname.match(video)) {
+    return await fetch(request, 
+        { cf: {cacheKey: customCacheKey, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 5, '400-499': 1, '500-599': 0 },},
+    }) 
+} else
+
+if (newRequest.pathname.match(manifest)) {
+    return await fetch(request, 
+        { cf: {cacheKey: customCacheKey, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 5, '400-499': 1, '500-599': 0 },},
+    })
+} else
+
+if (newRequest.pathname.match(image)) {
+    return await fetch(request, 
+        { cf: {cacheKey: customCacheKey, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 5, '400-499': 1, '500-599': 0 },},
+    })
+} else
+
+if (newRequest.pathname.match(frontEnd)) {
+    return await fetch(request, 
+        { cf: {cacheKey: customCacheKey, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 5, '400-499': 1, '500-599': 0 },},
+    })
+} else
+
+if (newRequest.pathname.match(audio)) {
+    return await fetch(request, 
+        { cf: {cacheKey: customCacheKey, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 5, '400-499': 1, '500-599': 0 },},
+    })
+} else
+
+if (newRequest.pathname.match(directPlay)) {
+    return await fetch(request, 
+        { cf: {cacheKey: customCacheKey, cacheEverything: true, cacheTtlByStatus: { '200-299': 86400, '300-399': 5, '400-499': 1, '500-599': 0 },},
+    })
+} else
+
+return fetch(request)
+
 }
 
-addEventListener('fetch', event => {
-    return event.respondWith(handleRequest(event.request))
-})
