8b7c0347897f39cad91a9e84d05a8f64d5793dca
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-03-15 15:00:34 +0800

    Changed TTLs

--- jellyfin.js
+++ jellyfin.js
@@ -8,12 +8,12 @@ const newRequest = new URL(request.url)
 const customCacheKey = newRequest.hostname + newRequest.pathname
 
 const cacheAssets = [
-    {asset: 'video', key: customCacheKey, regex: /^.*\.(m4s|mp4|ts|avi|mpeg|mpg|mkv|bin|webm|vob|flv|m2ts|mts|3gp|m4v|wmv|qt)/, info: -1, ok: 3, redirects: 3, clientError: 10, serverError: 0 },
-    {asset: 'image', key: customCacheKey, regex: /^.*\.(jpeg|jpg|png|dng|tiff|webp|gif)/, info: -1, ok: 3, redirects: 30, clientError: 10, serverError: 0 },
-    {asset: 'frontEnd', key: customCacheKey, regex: /^.*\.(css|js)/, info: -1, ok: 3, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'video', key: customCacheKey, regex: /^.*\.(m4s|mp4|ts|avi|mpeg|mpg|mkv|bin|webm|vob|flv|m2ts|mts|3gp|m4v|wmv|qt)/, info: -1, ok: 86400, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'image', key: customCacheKey, regex: /^.*\.(jpeg|jpg|png|dng|tiff|webp|gif)/, info: -1, ok: 86400, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'frontEnd', key: customCacheKey, regex: /^.*\.(css|js)/, info: -1, ok: 3, redirects: 3600, clientError: 10, serverError: 0 },
     {asset: 'audio', key: customCacheKey, regex: /^.*\.(flac|aac|mp3|alac|aiff|wav|ogg|aiff|opus|ape|wma|3gp)/, info: -1, ok: 86400, redirects: 30, clientError: 10, serverError: 0 },
-    {asset: 'directPlay', key: customCacheKey, regex: /^.*(\/Download|\/Audio)/, info: -1, ok: 3, redirects: 30, clientError: 10, serverError: 0 },
-    {asset: 'manifest', key: customCacheKey, regex: /^.*\.(m3u8|mpd)/, info: -1, ok: 3, redirects: 30, clientError: 10, serverError: 0 }
+    {asset: 'directPlay', key: customCacheKey, regex: /^.*(\/Download|\/Audio)/, info: -1, ok: 86400, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'manifest', key: customCacheKey, regex: /^.*\.(m3u8|mpd)/, info: -1, ok: 3, redirects: 2, clientError: 1, serverError: 0 }
 ]
 
 const cacheAssets_match = cacheAssets.find( ({regex}) => newRequest.pathname.toLowerCase().match(regex))
