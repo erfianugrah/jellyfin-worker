262e267eb82c60a23a7a6486b377d841fb320dde
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-03-20 07:53:39 +0800

    Fixed Spelling

--- jellyfin.js
+++ jellyfin.js
@@ -6,12 +6,12 @@ async function handleRequest(request) {
 
 const newRequest = new URL(request.url)
 const customCacheKey = newRequest.hostname + newRequest.pathname
-const queryCackeKey = newRequest.hostname + newRequest.pathname + newRequest.search
+const queryCacheKey = newRequest.hostname + newRequest.pathname + newRequest.search
 
 const cacheAssets = [
     {asset: 'video', key: customCacheKey, regex: /^.*\.(m4s|mp4|ts|avi|mpeg|mpg|mkv|bin|webm|vob|flv|m2ts|mts|3gp|m4v|wmv|qt)/, info: 0, ok: 86400, redirects: 30, clientError: 10, serverError: 0 },
     {asset: 'image', key: customCacheKey, regex: /^.*\.(jpeg|jpg|png|dng|tiff|webp|gif)/, info: 0, ok: 86400, redirects: 30, clientError: 10, serverError: 0 },
-    {asset: 'frontEnd', key: queryCackeKey, regex: /^.*\.(css|js)/, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'frontEnd', key: queryCacheKey, regex: /^.*\.(css|js)/, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
     {asset: 'audio', key: customCacheKey, regex: /^.*\.(flac|aac|mp3|alac|aiff|wav|ogg|aiff|opus|ape|wma|3gp)/, info: 0, ok: 86400, redirects: 30, clientError: 10, serverError: 0 },
     {asset: 'directPlay', key: customCacheKey, regex: /^.*(\/Download|\/Audio)/, info: 0, ok: 86400, redirects: 30, clientError: 10, serverError: 0 },
     {asset: 'manifest', key: customCacheKey, regex: /^.*\.(m3u8|mpd)/, info: 0, ok: 3, redirects: 2, clientError: 1, serverError: 0 }
