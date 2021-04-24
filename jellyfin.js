87ff6f27b9ef7a307a8e3f766ee0d3ef949d295b
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-04-24 14:17:52 +0800

    Changed to object

--- jellyfin.js
+++ jellyfin.js
@@ -8,16 +8,16 @@ const newRequest = new URL(request.url)
 const customCacheKey = newRequest.hostname + newRequest.pathname
 const queryCacheKey = newRequest.hostname + newRequest.pathname + newRequest.searchParams
 
-const cacheAssets = [
+const cacheAssets = {
     {asset: 'video', key: customCacheKey, regex: /^.*\.(m4s|mp4|ts|avi|mpeg|mpg|mkv|bin|webm|vob|flv|m2ts|mts|3gp|m4v|wmv|qt)/, info: 0, ok: 31556952, redirects: 30, clientError: 10, serverError: 0 },
     {asset: 'image', key: queryCacheKey, regex: /^.*\.(jpg|jpeg|png|bmp|pict|tif|tiff|webp|gif|heif|exif|bat|bpg|ppm|pgn|pbm|pnm)/, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
     {asset: 'frontEnd', key: queryCacheKey, regex: /^.*\.(css|js)/, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
     {asset: 'audio', key: customCacheKey, regex: /^.*\.(flac|aac|mp3|alac|aiff|wav|ogg|aiff|opus|ape|wma|3gp)/, info: 0, ok: 31556952, redirects: 30, clientError: 10, serverError: 0 },
     {asset: 'directPlay', key: customCacheKey, regex: /^.*(\/Download|\/Audio)/, info: 0, ok: 31556952, redirects: 30, clientError: 10, serverError: 0 },
     {asset: 'manifest', key: customCacheKey, regex: /^.*\.(m3u8|mpd)/, info: 0, ok: 3, redirects: 2, clientError: 1, serverError: 0 }
-]
-
-const cacheAssets_match = cacheAssets.find( ({regex}) => newRequest.pathname.toLowerCase().match(regex))
+}
+const cacheAssets_match = newRequest.pathname.toLowerCase().match(cacheAssets[regex])
+//const cacheAssets_match = cacheAssets.find( ({regex}) => newRequest.pathname.toLowerCase().match(regex))
 const cache = cacheAssets_match ? cacheAssets_match : {}
 
 return await fetch(request,
