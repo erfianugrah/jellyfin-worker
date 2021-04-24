9dc1e2169eb0533f638cf0c59173ae90f29985a3
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-04-24 15:13:46 +0800

    Reverted

--- jellyfin.js
+++ jellyfin.js
@@ -8,29 +8,17 @@ const newRequest = new URL(request.url)
 const customCacheKey = newRequest.hostname + newRequest.pathname
 const queryCacheKey = newRequest.hostname + newRequest.pathname + newRequest.searchParams
 
-const video = /^.*\.(m4s|mp4|ts|avi|mpeg|mpg|mkv|bin|webm|vob|flv|m2ts|mts|3gp|m4v|wmv|qt)/
-const image = /^.*\.(jpg|jpeg|png|bmp|pict|tif|tiff|webp|gif|heif|exif|bat|bpg|ppm|pgn|pbm|pnm)/
-const frontEnd = /^.*\.(css|js)/
-const audio = /^.*\.(flac|aac|mp3|alac|aiff|wav|ogg|aiff|opus|ape|wma|3gp)/
-const directPlay = /^.*(\/Download|\/Audio)/
-const manifest = /^.*\.(m3u8|mpd)/
+const cacheAssets = [
+    {asset: 'video', key: customCacheKey, regex: /^.*\.(m4s|mp4|ts|avi|mpeg|mpg|mkv|bin|webm|vob|flv|m2ts|mts|3gp|m4v|wmv|qt)/, info: 0, ok: 31556952, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'image', key: queryCacheKey, regex: /^.*\.(jpeg|jpg|png|dng|tiff|webp|gif)/, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'frontEnd', key: queryCacheKey, regex: /^.*\.(css|js)/, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'audio', key: customCacheKey, regex: /^.*\.(flac|aac|mp3|alac|aiff|wav|ogg|aiff|opus|ape|wma|3gp)/, info: 0, ok: 31556952, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'directPlay', key: customCacheKey, regex: /^.*(\/Download|\/Audio)/, info: 0, ok: 31556952, redirects: 30, clientError: 10, serverError: 0 },
+    {asset: 'manifest', key: customCacheKey, regex: /^.*\.(m3u8|mpd)/, info: 0, ok: 3, redirects: 2, clientError: 1, serverError: 0 }
+]
 
-const cacheAssets_regex = { video, image, frontEnd, audio, directPlay, manifest }
-const cacheAssets_match = newRequest.pathname.toLowerCase().match(cacheAssets_regex)
-
-const cacheAssets = {
-    video: { key: customCacheKey, info: 0, ok: 31556952, redirects: 30, clientError: 10, serverError: 0 },
-    image: { key: queryCacheKey, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
-    frontEnd: { key: queryCacheKey, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
-    audio: { key: customCacheKey, info: 0, ok: 31556952, redirects: 30, clientError: 10, serverError: 0 },
-    directPlay: { key: customCacheKey, info: 0, ok: 31556952, redirects: 30, clientError: 10, serverError: 0 },
-    manifest: { key: customCacheKey, info: 0, ok: 3, redirects: 2, clientError: 1, serverError: 0 }
-}
-
-//const cacheAssets_match = cacheAssets_regex ? cacheAssets : {}
-//const cacheAssets_match = { video, image, image, frontend, audio, directPlay, manifest }
-//const cacheAssets_match = cacheAssets.find( ({regex}) => newRequest.pathname.toLowerCase().match(regex))
-const cache = cacheAssets_match ? cacheAssets : {}
+const cacheAssets_match = cacheAssets.find( ({regex}) => newRequest.pathname.toLowerCase().match(regex))
+const cache = cacheAssets_match ? cacheAssets_match : {}
 
 const newResponse = await fetch(request,
         { cf:
